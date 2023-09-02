2e5a315016ea3f9c76b4f8cba19c75341e2ed1f7
MIKHAIL ZHACHKO <42667303+Gargoros@users.noreply.github.com>
2023-09-02 14:15:22 +0300

    feat: added constants (#11)

---
+++ hotel_app/lib/src/core/constants/app_text_constants.dart
@@ -0,0 +1,52 @@
+class AppTextConst {
+  // hotel screen
+  static const String hotelTitle = "Отель";
+  static const String aboutTheHotel = "Об отеле";
+  static const String comfort = "Удобства";
+  static const String included = "Что включено";
+  static const String notIncluded = "Что не включено";
+  static const String mostNecessary = "Самое необходимое";
+  static const String goToRoomsButton = "К выбору номера";
+  static const String priceStart = "от";
+  static const String cashSign = "₽";
+
+  // rooms screen
+  static const String moreRoomDetails = "Подробнее о номере";
+  static const String roomSelect = "Выбрать номер";
+
+  // booking screen
+  static const String bookingTitle = "Бронирование";
+  static const String departureFrom = "Вылет из";
+  static const String hotelLocation = "Страна, город";
+  static const String date = "Даты";
+  static const String numberOfNights = "Кол-во ночей";
+  static const String hotelName = "Отель";
+  static const String roomType = "Номер";
+  static const String foodType = "Питание";
+  static const String infoAboutTheBuyer = "Информация о покупателе";
+  static const String buyerPhoneNumber = "Номер телефона";
+  static const String buyerEmil = "Почта";
+  static const String dataShareInfo =
+      "Эти данные никому не передаются. После оплаты мы вышли чек на указанный вами номер и почту";
+  static const String firstName = "Имя";
+  static const String surName = "Фамилия";
+  static const String dateOfBirth = "Дата рождения";
+  static const String citizenship = "Гражданство";
+  static const String passportNumber = "Номер загранпаспорта";
+  static const String validityPeriodOfPassport = "Срок действия загранпаспорта";
+  static const String tour = "Тур";
+  static const String fuelCollection = "Топливный сбор";
+  static const String serviceFee = "Сервисный сбор";
+  static const String toBePaid = "К оплате";
+  static const String toPayButton = "Оплатить";
+  static const String firstTourist = "Первый турист";
+  static const String secondTourist = "Второй турист";
+  static const String addTourist = "Добавить туриста";
+
+  // paid screen
+  static const String orderIsPaid = "Заказ оплачен";
+  static const String orderAccepted = "Ваш заказ принят в работу";
+  static const String orderAcceptedDetails =
+      "Подтверждение заказа №104893 может занять некоторое время (от 1 часа до суток). Как только мы получим ответ от туроператора, вам на почту придет уведомление.";
+  static const String paidButton = "Супер!";
+}
